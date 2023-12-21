6ca473f6f2f4291b9a2534824aaa5702d28efeac
Crazy Diamond <chumi0902@gmail.com>
2023-12-21 14:42:00 +0800

    Back Entry & File add

--- src/router/index.js
+++ src/router/index.js
@@ -47,11 +47,45 @@ const router = createRouter({
     },
 //後台連結
     {
-      path: '/BackEntry',
-      name: 'BackEntry',
-      component: () => import('../views/BackEnd/BackEntry.vue')
+      path: '/BackLogin',
+      name: 'BackLogin',
+      component: () => import('../views/BackEnd/BackLogin.vue')
+    },
+    {
+      path: '/BackManagement',
+      name: 'BackManagement',
+      component: () => import('../views/BackEnd/BackManagement.vue')
+    },
+    {
+      path: '/BackSingle',
+      name: 'BackSingle',
+      component: () => import('../views/BackEnd/BackRoom/BackSingle.vue')
+    },
+    {
+      path: '/BackDouble',
+      name: 'BackDouble',
+      component: () => import('../views/BackEnd/BackRoom/BackDouble.vue')
+    },
+    {
+      path: '/BackFamily',
+      name: 'BackFamily',
+      component: () => import('../views/BackEnd/BackRoom/BackFamily.vue')
+    },
+    {
+      path: '/BackTourist',
+      name: 'BackTourist',
+      component: () => import('../views/BackEnd/BackTourist.vue')
+    },
+    {
+      path: '/BackActivity',
+      name: 'BackActivity',
+      component: () => import('../views/BackEnd/BackActivity.vue')
+    },
+    {
+      path: '/BackAbout',
+      name: 'BackAbout',
+      component: () => import('../views/BackEnd/BackAbout.vue')
     },
   ]
 })
-
 export default router
---
+++ src/views/BackEnd/BackRoom/BackFamily.vue
@@ -0,0 +1,18 @@
+<script>
+export default{
+    data(){
+        return{
+
+        }
+    },
+}
+</script>
+
+<template>
+
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>
