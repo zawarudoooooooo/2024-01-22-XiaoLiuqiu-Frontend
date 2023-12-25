001cbc8d2a905516840ad0d25facbfd97a1462ca
Crazy Diamond <chumi0902@gmail.com>
2023-12-25 11:26:33 +0800

    Footer Add

--- src/views/FrontEnd/FrontAbout.vue
+++ src/views/FrontEnd/FrontAbout.vue
@@ -1,18 +1,28 @@
 <script>
+import Footer from '../../components/Footer.vue';
 export default{
     data(){
         return{
 
         }
     },
+    components:{
+        Footer
+    }
 }
 </script>
 
 <template>
+    <div class="content">
 
+    </div>
 
+<Footer />
 </template>
 
 <style lang="scss" scoped>
-
+    .content{
+        width: 100vw;
+        height: 70vh;
+    }
 </style>
