b3d2f32b2a9048030bd5b1757c03c0dd8a734e48
Crazy Diamond <chumi0902@gmail.com>
2024-01-10 17:12:46 +0800

    alert style change

--- src/views/FrontEnd/FrontSignUp.vue
+++ src/views/FrontEnd/FrontSignUp.vue
@@ -1,6 +1,7 @@
 <script>
 import axios from 'axios';
 import Footer from '../../components/Footer.vue';
+import swal from 'sweetalert';
 export default {
     data() {
         return {
@@ -12,7 +13,7 @@ export default {
         }
     },
     methods: {
-        goFrontEntry() {
+        login() {
             axios({
                 url: 'http://localhost:8080/member/signUp',
                 method: 'POST',
@@ -29,10 +30,10 @@ export default {
             }).then(res => {
                 console.log(res.data)
                 if(this.account&&this.pwd&& this.memberName&&this.memberPhone&&this.memberEmail==""){
-                    window.alert("必填欄位不得為空白")
+                    swal("必填欄位", "不得為空白", "error");
                 }else{
-                    window.alert("發送成功並回到主頁，請重新登入")
-                this.$router.push('/')
+                    swal("註冊成功", "請重新登入", "success");
+                    this.$router.push('/FrontLogin')
                 }
                 
             }).catch(error => {
@@ -46,14 +47,17 @@ export default {
             });
 
         },
-        test(){
-            console.log(this.account);
-            console.log(this.pwd);
-            console.log(this.memberName);
-            console.log(this.memberPhone);
-            console.log(this.memberEmail);
+        goEntry(){
             this.$router.push('/')
-        }
+        },
+        // test(){
+        //     console.log(this.account);
+        //     console.log(this.pwd);
+        //     console.log(this.memberName);
+        //     console.log(this.memberPhone);
+        //     console.log(this.memberEmail);
+        //     this.$router.push('/')
+        // }
     },
     components: {
         Footer
@@ -66,11 +70,9 @@ export default {
         <div class="title">
             <p>註冊</p>
         </div>
-        <button type="button" @click="test()">取消</button>
-        <button type="button" @click="goFrontEntry()">註冊</button>
         <div class="account">
             <p>帳號</p>
-            <input type="text" v-model="this.account" placeholder="this is login account">
+            <input type="text" v-model="this.account" placeholder="請設定帳號">
         </div>
         <div class="name">
             <p>姓名</p>
@@ -89,8 +91,8 @@ export default {
             <input type="text" v-model="this.memberEmail" placeholder="請輸入email">
         </div>
         <div class="buttonArea">
-            <button type="button" @click="test()">取消</button>
-            <button type="button" @click="goFrontEntry()">註冊</button>
+            <button type="button" @click="goEntry()">取消</button>
+            <button type="button" @click="login()">註冊</button>
         </div>
     </div>
     <Footer />
@@ -99,10 +101,9 @@ export default {
 <style lang="scss" scoped>
 .content {
     width: 20vw;
-    height: 67vh;
+    height: 68vh;
     margin: auto;
-    margin-top: 4vmin;
-
+    margin-top: 3vmin;
     input {
         width: 20vw;
         height: 5vh;
@@ -111,20 +112,17 @@ export default {
         outline: none;
         background-color: #D9D9D9;
         padding-left: 2vmin;
-        margin-bottom: 2vmin;
+        margin-bottom: 1vmin;
     }
-
     .title {
         font-size: 28pt;
         font-weight: bold;
         color: #82AAE3;
         text-align: center;
-
         p {
             margin-bottom: 0.5vmin;
         }
     }
-
     .account {
         p {
             margin: 0;
@@ -132,7 +130,13 @@ export default {
             color: #797A7E;
         }
     }
-
+    .name {
+        p {
+            margin: 0;
+            font-size: 14pt;
+            color: #797A7E;
+        }
+    }
     .password {
         p {
             margin: 0;
@@ -140,7 +144,6 @@ export default {
             color: #797A7E;
         }
     }
-
     .phone {
         p {
             margin: 0;
@@ -148,7 +151,6 @@ export default {
             color: #797A7E;
         }
     }
-
     .email {
         p {
             margin: 0;
@@ -156,13 +158,11 @@ export default {
             color: #797A7E;
         }
     }
-
     .buttonArea {
         width: 20vw;
         display: flex;
         justify-content: space-around;
         margin-top: 3vmin;
-
         button {
             width: 8vw;
             height: 5vh;
