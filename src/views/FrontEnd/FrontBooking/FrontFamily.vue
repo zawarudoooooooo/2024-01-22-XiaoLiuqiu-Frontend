4ac10332f70ac00aa1cecafe6e8584d1fc8d1487
Crazy Diamond <chumi0902@gmail.com>
2024-01-12 16:25:13 +0800

    button style change

--- src/views/FrontEnd/FrontBooking/FrontFamily.vue
+++ src/views/FrontEnd/FrontBooking/FrontFamily.vue
@@ -3,14 +3,12 @@ import Footer from '../../../components/Footer.vue';
 export default{
     data(){
         return{
-
         }
     },
     methods:{
         booking(){
             this.$router.push('/FamilyBooking')
         }
-
     },
     components:{
         Footer
@@ -57,7 +55,6 @@ export default{
         margin-top: 6vmin;
         .date{
             width: 60vw;
-            //border: 1px solid black;
             display: flex;
             justify-content: space-around;
             align-content: center;
@@ -113,7 +110,6 @@ export default{
             }
             .text{
                 height: 23vh;
-                //border: 1px solid black;
                 hr{
                     margin: 0;
                 }
