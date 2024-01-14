3a0d5c92bf4eb6214b7ba8d381466837814335ec
Crazy Diamond <chumi0902@gmail.com>
2024-01-14 14:32:17 +0800

    meal add

--- src/views/FrontEnd/FrontMeal.vue
+++ src/views/FrontEnd/FrontMeal.vue
@@ -17,16 +17,285 @@ export default{
 
 <template>
     <div class="content">
+        <div class="title">
+            <p>美食</p>
+        </div>
+        <hr>
+        <div class="meal">
+            <div class="card mb-3" style="max-width: 46%;">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="../../../public/meal/turtle.jpg" style="width: 100%;height: 100%;" alt="...">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h4 class="card-title">海龜燒</h4>
+                            <p class="card-text">
+                                <ul>
+                                    <li>
+                                        店家 : 西晒咖啡
+                                    </li>
+                                    <li>
+                                        營業時間 : 9:00-18:00
+                                    </li>
+                                    <li>
+                                        地址 : 屏東縣琉球鄉觀光港路30號
+                                    </li>
+                                    <li>
+                                        電話 : 0965-105-988
+                                    </li>
+                                    <li>
+                                        海龜燒販賣時間 : 13:00-售完為止
+                                    </li>
+                                </ul>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
+            <div class="card mb-3" style="max-width: 46%;">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="../../../public/meal/cheese.jpg" style="width: 100%;height: 100%;" alt="...">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h4 class="card-title">起司捲</h4>
+                            <p class="card-text">
+                                <ul>
+                                    <li>
+                                        店家 : YOYO起司捲
+                                    </li>
+                                    <li>
+                                        營業時間 : 週一至週五及週日 14:00-17:00 週六 13:00-17:00
+                                    </li>
+                                    <li>
+                                        地址 : 屏東縣琉球鄉三民路169號
+                                    </li>
+                                    <li>
+                                        電話 : 0916-296-929
+                                    </li>
+                                </ul>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card mb-3" style="max-width: 46%;">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="../../../public/meal/ice.jpg" style="width: 100%;height: 100%;" alt="...">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h4 class="card-title">冰品</h4>
+                            <p class="card-text">
+                                <ul>
+                                    <li>
+                                        店家 : 海找冰
+                                    </li>
+                                    <li>
+                                        營業時間 : 11:00～17:30，星期二公休
+                                    </li>
+                                    <li>
+                                        地址 : 屏東縣琉球鄉忠孝路1巷1-12號
+                                    </li>
+                                    <li>
+                                        不提供外帶，6歲以上低消100元。
+                                    </li>
+                                </ul>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card mb-3" style="max-width: 46%;">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="../../../public/meal/donut.jpg" style="width: 100%;height: 100%;" alt="...">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h4 class="card-title">可頌甜甜圈</h4>
+                            <p class="card-text">
+                                <ul>
+                                    <li>
+                                        店家 : 廟口點心站
+                                    </li>
+                                    <li>
+                                        營業時間 : 08:30～18:00（週二、三、四公休）
+                                    </li>
+                                    <li>
+                                        地址 : 屏東縣琉球鄉三民路277號
+                                    </li>
+                                    <li>
+                                        電話 : 08-861-4485
+                                    </li>
+                                </ul>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card mb-3" style="max-width: 46%;">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="../../../public/meal/smallcheese.jpg" style="width: 100%;height: 101%;" alt="...">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h4 class="card-title">起司餅</h4>
+                            <p class="card-text">
+                                <ul>
+                                    <li>
+                                        店家 : 小蝌蚪起司餅
+                                    </li>
+                                    <li>
+                                        營業時間 : 14:00～22:00
+                                    </li>
+                                    <li>
+                                        地址 : 屏東縣琉球鄉三民路210號
+                                    </li>
+                                    <li>
+                                        電話 : 0976-181-739
+                                    </li>
+                                </ul>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card mb-3" style="max-width: 46%;">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="../../../public/meal/mr.jpg" style="width: 100%;height: 100%;" alt="...">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h4 class="card-title">輕食</h4>
+                            <p class="card-text">
+                                <ul>
+                                    <li>
+                                        店家 : Mr.奶酥
+                                    </li>
+                                    <li>
+                                        營業時間 : 17:00~01:00
+                                    </li>
+                                    <li>
+                                        地址 : 屏東縣琉球鄉上杉路35號
+                                    </li>
+                                    <li>
+                                        電話 : 0979-559-568
+                                    </li>
+                                </ul>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card mb-3" style="max-width: 46%;">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="../../../public/meal/banto.jpg" style="width: 100%;height: 110%;" alt="...">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h4 class="card-title">海龜便當</h4>
+                            <p class="card-text">
+                                <ul>
+                                    <li>
+                                        店家 : 小川壽司海龜便當
+                                    </li>
+                                    <li>
+                                        營業時間 : 11:00~14:00 (每週四公休)
+                                    </li>
+                                    <li>
+                                        地址 : 屏東縣琉球鄉中山路61-25號1樓
+                                    </li>
+                                    <li>
+                                        電話 : 0933-781-737
+                                    </li>
+                                </ul>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card mb-3" style="max-width: 46%;">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="../../../public/meal/bula.jpg" style="width: 100%;height: 100%;" alt="...">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h4 class="card-title">咖椰吐司</h4>
+                            <p class="card-text">
+                                <ul>
+                                    <li>
+                                        店家 : Bula 冷奶油咖椰吐司
+                                    </li>
+                                    <li>
+                                        營業時間 : 17:00 - 22:00
+                                    </li>
+                                    <li>
+                                        地址 : 屏東縣琉球鄉民生路32號 
+                                    </li>
+                                    <li>
+                                        電話 : 0903-380-829
+                                    </li>
+                                </ul>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
     <Footer />
 </template>
 
 <style lang="scss" scoped>
-        .content{
-        width: 20vw;
-        height: 66vh;
+    .content{
+        width: 80vw;
+        height: 138vh;
         margin: auto;
         margin-top: 4vmin;
+        .title{
+            p{
+                font-size: 28pt;
+                font-weight: bold;
+                color: #82AAE3;
+                text-align: center;
+            }
+        }
+        hr{
+            margin-bottom: 3vmin;
+        }
+        .meal{
+            width: 80vw;
+            display: flex;
+            justify-content: space-around;
+            flex-wrap: wrap;
+            gap: 1vmin;
+            .card{
+                box-shadow: 1px 1px 1px lightgray;
+                img{
+                    transition: all ease 0.3s;
+                    &:hover{
+                        opacity: 0.7;
+                    }
+                    &:active{
+                        opacity: 1;
+                    } 
+                }
+            }
+        }
     }
 </style>
