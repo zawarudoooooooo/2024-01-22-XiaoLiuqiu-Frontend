d9ac60a6e760b8cc7c5e34aaaffe12d6e9abe95d
Jerrylac <j78779915@gmail.com>
2024-01-19 17:29:49 +0800

    doule

--- src/views/FrontEnd/FrontBooking/FrontDouble.vue
+++ src/views/FrontEnd/FrontBooking/FrontDouble.vue
@@ -1,17 +1,26 @@
 <script>
 import axios from 'axios';
 import Footer from '../../../components/Footer.vue';
+import { info } from 'sass';
 export default{
     data(){
         return{
             roomList:"",
-            // roomId:"",
+            roomId:"",
             // roomTypeId: "",
             // roomIntroduce:"",
             // roomTypeId2:"",
             roomName:"",
             roomPrice:"",
-            orderRoomId:[]
+            orderRoomId:[],
+            today:new Date,
+            startDate:"",
+            endDate:"",
+            mStartDate:"",
+            mEndDate:"",
+            searchInfo:[],
+            searchBo:false,
+            Info:[]
         }
     },
     mounted() {
@@ -19,7 +28,7 @@ export default{
         this.List.forEach(element => {
                 this.roomName=element.roomName
             });
-            console.log(this.roomName);
+            // console.log(this.roomName);
 
         axios({
             url:'http://localhost:8080/room/search',
@@ -62,8 +71,8 @@ export default{
             })
             // console.log(this.orderRoomId);
             })
-           
-        // console.log(this.List);
+            
+        console.log(this.List);
     },
     props:[
         "List"
@@ -73,16 +82,105 @@ export default{
             this.$router.push('/DoubleBooking')
         },
         roomIsopren(open,roomId){
-            console.log(open);
-            console.log(roomId);
+            // console.log(open);
+            // console.log(roomId);
+            let date=this.today.getUTCFullYear()+'-'+(this.today.getMonth()+1)+'-'+this.today.getDate()
+            this.roomId=""
+            this.startDate="" 
+            this.endDate=""
             this.orderRoomId.forEach(item=>{
-            
+                if(roomId!=item.roomId){
+                    return
+                }
+                this.roomId=item.roomId
+                let sDate=new Date(item.startDate)
+                let eDate=new Date(item.endDate)
+                this.startDate=sDate.getUTCFullYear()+'-'+(sDate.getMonth()+1)+'-'+sDate.getDate()
+                this.endDate=eDate.getUTCFullYear()+'-'+(eDate.getMonth()+1)+'-'+eDate.getDate()
             })
+            // console.log(this.roomId);
+            if(this.roomId==roomId){
+                if(this.endDate<=date||this.startDate<=date){
+                    return'已有人訂房';
+                }
+            }
             if(open){
                 return "空房"
             }
             return "整修中"
+        },
+        search(){
+
+            axios({
+            url:'http://localhost:8080/order/search',
+            method:'POST',
+            headers:{
+              'Content-Type':'application/json'
+            },
+            params:{
+            },
+            data:{
+                start_date:this.mStartDate,
+                end_date:this.mEndDate
+            },
+          }).then(res=>{
+            // console.log(res.data.orderList);
+            let date=this.today.getUTCFullYear()+'-'+(this.today.getMonth()+1)+'-'+this.today.getDate()
+            this.startDate="" 
+            this.endDate=""
+
+                let mEndDate=new Date(this.mEndDate)
+                let mStartDate=new Date(this.mStartDate)
+                let enDate=mEndDate.getUTCFullYear()+'-'+(mEndDate.getMonth()+1)+'-'+mEndDate.getDate()
+                let stDate=mStartDate.getUTCFullYear()+'-'+(mStartDate.getMonth()+1)+'-'+mStartDate.getDate()
+                res.data.orderList.forEach(item=>{
+                    // console.log(item);
+                    let sDate=new Date(item.startDate)
+                    let eDate=new Date(item.endDate)
+                    
+                    this.startDate=sDate.getUTCFullYear()+'-'+(sDate.getMonth()+1)+'-'+sDate.getDate()
+                    this.endDate=eDate.getUTCFullYear()+'-'+(eDate.getMonth()+1)+'-'+eDate.getDate()
+                    JSON.parse(item.roomId).forEach(itemRoom=>{
+                        // console.log(itemRoom);
+                            this.searchInfo=[]
+                        
+                        this.roomList.forEach(list=>{
+                            // console.log(list);
+                            // if(itemRoom.roomId.charAt(0) === 'A'){
+                                // console.log(itemRoom);
+                                // console.log(list);
+                                
+                                if(this.mStartDate==""||this.mStartDate==""){
+                                    
+                                    this.searchInfo.push(list)
+                                    return
+                                }
+                                
+                                if(itemRoom.roomId==list.roomId){
+                                    
+                                    if(enDate<=this.endDate&&stDate>=this.startDate){
+                                        console.log(list);  
+                                    }
+                                    
+                                    return
+                                }
+                                
+                                this.searchInfo.push(list)
+                                // console.log(this.searchInfo);
+                                
+                            // }
+                        })
+                        
+                        // console.log(this.Info);
+                       
+                    });
+                   
+                })
+                console.log(this.searchInfo);
+                this.roomList=this.searchInfo;
+            })
         }
+        
 
     },
     components:{
@@ -95,22 +193,23 @@ export default{
     
     
     <div class="content">
-        <div class="date">
+        <!-- <div class="date">
             
             <div class="checkin">
                 <p>入住日期</p>
-                <input type="date">
+                <input type="date" v-model="this.mStartDate">
             </div>
             <div class="checkout">
                 <p>退房日期</p>
-                <input type="date">
+                <input type="date" v-model="this.mEndDate">
             </div>
             <div>
             </div>
             <button type="button">回上頁</button>
-            <button type="button" >搜尋</button>
-        </div>
-        <div class="show" v-for="item in this.roomList" >
+            <button type="button" @click="search()" >搜尋</button>
+        </div> -->
+        <button type="button">回上頁</button>
+        <div class="show" v-for="item in this.roomList">
             <img src="../../../../room/double.jpg" alt="">
             <div class="text">
                 <div class="name" >
@@ -127,6 +226,7 @@ export default{
                 <button type="button" @click="booking()">訂購</button>
             </div>
         </div>
+        
     </div>
 </template>
 
