ea08c5ddbf7ed919d479a48616348037f9fde338
Crazy Diamond <chumi0902@gmail.com>
2024-01-31 15:53:02 +0800

    rwd

--- src/views/FrontEnd/Other/FrontOther.vue
+++ src/views/FrontEnd/Other/FrontOther.vue
@@ -22,11 +22,15 @@ export default{
                 <div class="topic">
                     <h2>Q : 小琉球天氣 ?</h2>
                 </div>
+                <hr>
                 <div class="answer">
                     <p>
-                        小琉球四季炎熱， 平均海溫終年都維持在 25℃ 以上，較台灣其他地區高，
+                        小琉球四季炎熱， 平均海溫終年都維持在 25℃ 以上。
+                        <br>
                         除了颱風之外，風浪也較為平靜。
-                        因緯度較低又位於台灣西半部外島，較不受東北季風影響，
+                        <br>
+                        因緯度較低又位於台灣西半部外島，較不受東北季風影響。
+                        <br>
                         四季溫度變化較小，屬於熱帶季風氣候。
                     </p>
                 </div>
@@ -36,12 +40,16 @@ export default{
                 <div class="topic">
                     <h2>Q : 冬天適合去小琉球嗎 ?</h2>
                 </div>
+                <hr>
                 <div class="answer">
                     <p>
-                        當地四季及海溫較為平穩，推薦大家可以在冬季前往小琉球遊玩。
-                        人潮也比較少，不像夏季的酷熱，較舒適宜人。
-                        甚至可以選擇每三年舉辦一次的迎王祭，時間約農曆10月，為期一週的慶典活動。
-                        小琉球人多會在時間返鄉，居民總動員參與幫忙，旅客也可以一睹小琉球這場宗教盛事。
+                        當地四季及海溫較為平穩，推薦大家可以在冬季前往。
+                        <br>
+                        人潮較少，不像夏季酷熱，較舒適宜人。
+                        <br>
+                        甚至可以選擇每三年舉辦一次的迎王祭，
+                        <br>
+                        時間約農曆10月，為期一週的慶典活動。
                     </p>
                 </div>
             </div>
@@ -50,9 +58,13 @@ export default{
                 <div class="topic">
                     <h2>Q : 小琉球有雨季嗎 ?</h2>
                 </div>
+                <hr>
                 <div class="answer">
                     <p>
-                        雖然小琉球氣候較穩定，但5-9月為本地的雨季，天氣仍有變化莫測的時候。
+                        雖然小琉球氣候較穩定，但5-9月為本地的雨季。
+                        <br>
+                        天氣仍有變化莫測的時候。
+                        <br>
                         推薦出發前做好功課，避免向隅。
                     </p>
                 </div>
@@ -62,9 +74,14 @@ export default{
                 <div class="topic">
                     <h2>Q : 不會游泳，能夠玩水上活動嗎 ?</h2>
                 </div>
+                <hr>
                 <div class="answer">
                     <p>
-                        SUP和獨木舟都會穿救生衣。所以即使不會游泳，也能盡情享受。
+                        大部分的水上活動都會穿救生衣。
+                        <br>
+                        所以即使不會游泳，也能盡情享受。
+                        <br>
+                        活動體驗時，也會有專業教練在場協助。
                     </p>
                 </div>
             </div>
@@ -73,14 +90,20 @@ export default{
                 <div class="topic">
                     <h2>Q : 水上活動的準備 ?</h2>
                 </div>
+                <hr>
                 <div class="answer">
                     <p>
                         建議穿著輕便舒適、透氣快乾的衣物。
-                        亦可內穿泳衣，減少需要更衣的時間。
-                        潛水時，即可直接換上潛水服，或穿上蛙鞋即可下海。
-                        另外應準備替換衣物、毛巾、收納袋等用品。
-                        可以帶著瓶裝水、小點心、水果零食等，隨時補充水分與體力。 
-                        容易暈車、暈船的朋友，建議於活動開始前半小時服用暈船藥，並於活動前一晚保持充足睡眠，以免暈浪產生不適。
+                        <br>
+                        亦可直接穿泳衣，減少更衣的時間。
+                        <br>
+                        應準備替換衣物、毛巾、收納袋等用品。
+                        <br>
+                        容易暈車、暈船的朋友，
+                        <br>
+                        建議於活動開始前半小時服用暈船藥，
+                        <br>
+                        並於活動前一晚保持充足睡眠，以免暈浪產生不適。
                     </p>
                 </div>
             </div>
@@ -89,33 +112,73 @@ export default{
                 <div class="topic">
                     <h2>Q : 浮潛需要證照嗎 ?</h2>
                 </div>
+                <hr>
                 <div class="answer">
                     <p>
-                        浮潛雖然不需要取得潛水證照即可進行，但還是有一定的專業性與危險性。
-                        建議尋找專業的店家和教練提供服務，讓您玩得盡興，安全也有保障。
-                        浮潛沒有特別的年齡限制，從老到小都可以參加。不過需要一定的體力，不建議行動不便者參加，患有高血壓、癲癇等病況者亦不適合。
+                        浮潛不需取得潛水證照即可進行，但還是有專業性與危險性。
+                        <br>
+                        建議尋找專業的店家和教練提供服務，
+                        <br>
+                        讓您玩得盡興，安全也有保障。
+                        <br>
+                        浮潛沒有特別的年齡限制，從老到小都可以參加。
+                        <br>
+                        不過需要一定的體力，不建議行動不便者參加，
+                        <br>
+                        患有高血壓、癲癇等病況者亦不適合。
                     </p>
                 </div>
             </div>
 
-            <div class="list">
+            <div class="list-last">
                 <div class="topic">
-                    <h2>水上活動守則</h2>
+                    <h2>※水上活動守則※</h2>
                 </div>
+                <hr>
                 <div class="answer">
                     <p>
-                        浮潛雖然不需要取得潛水證照即可進行，但還是有一定的專業性與危險性。
-                        建議尋找專業的店家和教練提供服務，讓您玩得盡興，安全也有保障。
-                        浮潛沒有特別的年齡限制，從老到小都可以參加。不過需要一定的體力，不建議行動不便者參加，患有高血壓、癲癇等病況者亦不適合。
+                        綠蠵龜是小琉球海域中的重要海龜明星，屬於一級保育類動物，繁殖不易。
+                        <br>
+                        因為牠以海草、海藻類為食，葉綠素漸漸累積於脂肪中，導致其脂肪呈墨綠色，才取名為綠蠵龜。
+                        <br>
+                        小琉球潛水一睹海龜的機率很高，但在海洋的世界裡我們只是客人。
+                        <br>
+                        <br>
+                        請務必遵守看海龜的 7 個原則
+                        <br>
+                        <small>（資料來源：交通部觀光局大鵬灣國家風景區管理處）</small>
+                        <br>
+                        <ol>
+                            <li>
+                                不觸碰、不捉、不驚擾、不追逐、不圍繞海龜
+                            </li>
+                            <li>
+                                不要阻擋海龜的去路
+                            </li>
+                            <li>
+                                與海龜保持五公尺以上的距離，減少海龜的壓迫感
+                            </li>
+                            <li>
+                                不打擾在休息、睡覺的海龜
+                            </li>
+                            <li>
+                                減少使用防曬乳，或選擇不傷害珊瑚的防曬乳
+                            </li>
+                            <li>
+                                不餵食海龜
+                            </li>
+                            <li>
+                                除了自身遵守看龜守則外，提醒同行下水者一起遵守，如果是跟著潛水或浮潛教練，也可以提出一樣的注意事項
+                            </li>
+                        </ol>
+                        <small>
+                            小提醒：由於海龜易受驚嚇，若追逐或觸摸，會使牠加速游泳，造成衝撞受傷或死亡，
+                            因此看見海龜請不要追逐、觸摸、驚嚇，否則可能吃上最高 30 萬元罰金。
+                        </small>
                     </p>
                 </div>
             </div>
-
-
-
-
         </div>
-
     </div>
     <Footer />
 </template>
@@ -123,7 +186,7 @@ export default{
 <style lang="scss" scoped>
     .content{
         width: 90vw;
-        height: 160vh;
+        height: 220vh;
         margin: auto;
         margin-top: 4vmin;
         hr{
@@ -138,9 +201,62 @@ export default{
             }
         }
         .show{
+            width: 85vw;
+            margin: auto;
+            display: flex;
+            justify-content: space-around;
+            flex-wrap: wrap;
             .list{
+                width: 40vw;
+                padding: 3vmin 2vmin 3vmin 3vmin;
                 background-color: white;
                 border-radius: 10px;
+                margin-bottom: 4vmin;
+                box-shadow: 1px 1px 1px gray;
+                transition: all linear 0.3s;
+                &:hover{
+                    opacity: 0.8;
+                    box-shadow: 8px 8px 2px 1px rgba(2, 40, 63, 0.2);
+                }
+                &:active{
+                    opacity: 1.0;
+                }
+                hr{
+                    margin-bottom: 1.5vmin;
+
+                }
+                .topic{
+                    text-align: center;
+                    h2{
+                        color: #4d4327;
+                    }
+                }
+                .answer{
+                    p{
+                        font-size: 15pt;
+                        color: #4d4327;
+                        text-align: center;
+                    }
+                }
+            }
+            .list-last{
+                width: 85vw;
+                padding: 3vmin 2vmin 3vmin 3vmin;
+                background-color: white;
+                border-radius: 10px;
+                margin-bottom: 5vmin;
+                box-shadow: 1px 1px 1px gray;
+                transition: all linear 0.3s;
+                &:hover{
+                    opacity: 0.8;
+                    box-shadow: 8px 8px 2px 1px rgba(2, 40, 63, 0.2);
+                }
+                &:active{
+                    opacity: 1.0;
+                }
+                hr{
+                    margin-bottom: 1.5vmin;
+                }
                 .topic{
                     text-align: center;
                 }
@@ -148,10 +264,138 @@ export default{
                     p{
                         font-size: 15pt;
                         color: #4d4327;
+                        text-align: center;
+                    }
+                    small{
+                        color: #4d4327;
+                    }
+                    li{
+                        color: #4d4327;
+                        font-size: 15pt;
                     }
                 }
             }
         }
     }
-
+    @media(max-width:1200px){
+        .content{
+            height: 117vh;
+            .show{
+                .list{
+                    margin-bottom: 2vmin;
+                    .topic{
+                        h2{
+                            font-size: 17pt;
+                        }
+                    }
+                    .answer{
+                        p{
+                            font-size: 10.5pt;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    @media(max-width:992px){
+        .content{
+            height: 114vh;
+            .show{
+                .list{
+                    .topic{
+                        h2{
+                            font-size: 14pt;
+                        }
+                    }
+                    .answer{
+                        p{
+                            font-size: 9pt;
+                        }
+                    }
+                }
+                .list-last{
+                    .answer{
+                        p{
+                            font-size: 11pt;
+                        }
+                        li{
+                            font-size: 12pt;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    @media(max-width:576px){
+        .content{
+            height: 95vh;
+            .title{
+                p{
+                    font-size: 22pt;
+                }
+            }
+            .show{
+                .list{
+                    hr{
+                        margin-top: 0.5vmin;
+                    }
+                    .topic{
+                        h2{
+                            font-size: 7pt;
+                        }
+                    }
+                    .answer{
+                        p{
+                            font-size: 4pt;
+                        }
+                    }
+                }
+                .list-last{
+                    .answer{
+                        p{
+                            font-size: 6pt;
+                        }
+                        li{
+                            font-size: 10pt;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    @media(max-width:414px){
+        .content{
+            height: 90vh;
+            .title{
+                p{
+                    font-size: 18pt;
+                }
+            }
+            .show{
+                .list{
+                    .topic{
+                        h2{
+                            font-size: 6pt;
+                            margin-bottom: 0.5vmin;
+                        }
+                    }
+                }
+                .list-last{
+                    .topic{
+                        h2{
+                            font-size: 12pt;
+                        }
+                    }
+                    .answer{
+                        li{
+                            font-size: 7pt;
+                        }
+                        small{
+                            font-size: 6pt;
+                        }
+                    }
+                }
+            }
+        }
+    }
 </style>
